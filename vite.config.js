6e3a1ba485c6470dfa791c36dcf7e0a837efbfe6
Avinash Kumar <avinashmadhukar4@gmail.com>
2025-04-04 18:25:23 +0530

    update vercel

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure this is set correctly for your deployment
+  base: "/",
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
